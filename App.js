f7171db4454c6de7659184a5b98d0bca10d0bea1
akbartels <akunrau@gmail.com>
2020-06-23 20:30:17 -0700

    added fonts

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import {
+  useFonts,
+  Inter_900Black,
+} from '@expo-google-fonts/inter';
+import { 
+  AguafinaScript_400Regular 
+} from '@expo-google-fonts/aguafina-script'
+import { 
+  AlmendraDisplay_400Regular 
+} from '@expo-google-fonts/almendra-display'
+
 
 export default function App() {
-  return (
+  const [name, setName] = useState("Amy");
+  const [person, setPerson] = useState({name: "Mario", age: 40})
+  
+  const clickHandler = () => {
+    setName("Nolan");
+    setPerson({name: "Luigi", age: 45})
+  }
+  
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+    AguafinaScript_400Regular,
+    AlmendraDisplay_400Regular, 
+  });
+
+
+  if (!fontsLoaded) {
+    return (
+      <Text>App loading...</Text>
+    );
+  } else {
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'AlmendraDisplay_400Regular' }}>My name is {name}</Text>
+      <Text style={{ fontFamily: 'AguafinaScript_400Regular',  }}>His name is {person.name} and his age is {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Update Name" onPress={clickHandler}/>
+      </View>
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +52,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    marginTop: 20,
+  },
+
 });
